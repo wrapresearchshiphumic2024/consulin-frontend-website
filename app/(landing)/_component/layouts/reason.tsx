e1477c2f8e95dffb678aefc278b70b05428abec3
Muhammad Faridz <faridzmuhamad679@gmail.com>
2024-10-02 11:13:56 +0700

    update navbar

--- app/(landing)/_component/layouts/reason.tsx
+++ app/(landing)/_component/layouts/reason.tsx
@@ -23,7 +23,7 @@ const reasons: CardReasonProps[] = [
 export default function Reason() {
   return (
     <section className="container mx-auto px-5 md:px-24 z-10 ">
-      <div className=" flex pt-[100px]    lg:mt-0  text-primary-custom_primary flex-col ">
+      <div className=" flex pt-[100px]  lg:mt-0  text-primary-custom_primary flex-col ">
         <center>
           <h1 className=" lg:text-[44px] text-4xl font-bold  md:mb-10">
             WHY CHOOSE US?

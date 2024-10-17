61ac3a2a58b29bb96c0cadbc0d91151c31a65611
Muhammad Faridz <faridzmuhamad679@gmail.com>
2024-10-17 09:04:38 +0700

    update meet

--- app/(landing)/coba/page.tsx
+++ app/(landing)/coba/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      {/* <Link href={"/meet?room_id=faridz-123"} target="_blank">
+    <div className="h-screen flex items-center justify-center">
+      <Link href={"/meet?room_id=faridz-123"} target="_blank">
         <Button>Link meet</Button>
       </Link>
       <Link href={"/meet?room_id=raya-124"} target="_blank">
@@ -11,7 +12,7 @@ export default function Home() {
       </Link>
       <Link href={"/meet?room_id=rohmad-125"} target="_blank">
         <Button>Link meet</Button>
-      </Link> */}
-    </>
+      </Link>
+    </div>
   );
 }
